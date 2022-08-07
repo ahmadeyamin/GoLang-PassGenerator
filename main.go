1e6fb612f8174c0f21234763989798fd100a7b23
Ahmad Eyamin <ahmadeyamin@gmail.com>
2022-08-07 12:50:39 +0600

    done

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"math/rand"
+	"strings"
+	"time"
+
 	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.GET("/pass", GeneratePassword)
+
+	r.Run(":8080")
+
+}
+
+func GeneratePassword(ctx *gin.Context) {
+	rand.Seed(time.Now().UnixNano())
+
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789" + "@$%&#@%$$^&$")
+
+	var p = strings.Builder{}
+
+	for i := 0; i < 20; i++ {
+		p.WriteRune(chars[rand.Intn(len(chars))])
+	}
+
+	ctx.String(200, p.String())
 }
